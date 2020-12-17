fb343244a0b41e842bf3c776029570d52adfab22
Elodie <ee@kaduceo.com>
2020-12-17 11:12:12 +0100

    Add copyright

--- drawing.py
+++ drawing.py
@@ -1,3 +1,21 @@
+"""
+drawing.py
+Copyright (C) 2020 Elodie Escriva, Kaduceo <elodie.escriva@kaduceo.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -14,9 +32,7 @@ def draw_avg_influence_per_class(influences, labels, labels_name):
             .rename(labels_name[c])
         )
         avg_infs_per_class = avg_infs_per_class.append(temp_avg)
-    avg_infs_per_class.T.plot.barh(
-        rot=0, title="Average attribute influence by class"
-    )
+    avg_infs_per_class.T.plot.barh(rot=0, title="Average attribute influence by class")
     plt.xlabel("Mean absolute influence", fontweight="bold")
     plt.show()
 
@@ -26,7 +42,9 @@ def draw_influence_instance(influences, label, labels_name, id_instance):
     print(influences.iloc[id_instance])
     print(infs_instance)
     colors = ["green" if x > 0 else "red" for x in infs_instance]
-    plt.title("Patient : {} ; Class : {}".format(id_instance, labels_name[label[id_instance]]))
+    plt.title(
+        "Patient : {} ; Class : {}".format(id_instance, labels_name[label[id_instance]])
+    )
     plt.xlabel("Influences")
     infs_instance.plot.barh(color=colors)
     plt.show()
--- utils.py
+++ utils.py
@@ -1,3 +1,24 @@
+"""
+Coalitional explanation method
+Copyright (C) 2020 Gabriel Ferrettini <gabriel.ferrettini@irit.fr>
+
+utils.py
+Copyright (C) 2020 Elodie Escriva, Kaduceo <elodie.escriva@kaduceo.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import numpy as np
 import pickle
 from sklearn.base import clone
